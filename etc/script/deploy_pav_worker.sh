b50b96ffa2ab8b24bd7e522e5e3909ef36b10b65
manuelmusio <manuel.musio@cmcc.it>
2021-11-22 11:12:47 +0100

    Add scripts for configuration and run of runtime

--- etc/script/deploy_pav_worker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-WORKERS_NAME=$1
-ESDM_PAV_RUNTIME_WORKER_PATH="/usr/local/ophidia/esdm-pav-runtime/bin/esdm_pav_runtime_worker -C"
-
-${ESDM_PAV_RUNTIME_WORKER_PATH} ${WORKERS_NAME} &
-
-exit 0
