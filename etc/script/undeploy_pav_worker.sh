95c3896f111770837287b8126e7ffc326e1fb8c7
manuelmusio <manuel.musio@cmcc.it>
2021-12-14 12:24:36 +0100

    Fix minor bugs

--- etc/script/undeploy_pav_worker.sh
+++ etc/script/undeploy_pav_worker.sh
@@ -25,7 +25,7 @@ if [ ${KILLER} == "bkill" ];
 then
     bkill -s SIGINT -J "worker_${JOB_TO_CANCEL}" > /dev/null
 else
-    kill -s SIGINT ${JOB_TO_CANCEL} > /dev/null
+    /usr/bin/kill -s SIGINT "$JOB_TO_CANCEL" > /dev/null
 fi
 
 exit 0
